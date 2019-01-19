aa64511c538b974623ae36daca7237e67066ec9e
Ali C <17682708+infernoarchon@users.noreply.github.com>
2019-01-19 12:16:45 -0500

    final

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: 'Swamptreegoat5!',
+        password: "password",
         database: 'burgers_db'
     })
 }
